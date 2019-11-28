586a92b6455782eaaf7205033e9819ab89d8c2ab
Julien Marrec <julien.marrec@gmail.com>
2019-11-28 10:49:47 +0100

    Initial commit with bash script to run jenkins via docker

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# https://jenkins.io/doc/book/installing/#downloading-and-running-jenkins-in-docker
+# -u root: Allow Jenkins to run as root to spawn docker containers to execute in your pipelines
+# --rm: remove container
+# -p 8080:8080: map local 8080 port (1st) to 8080 (2nd) in Jenkins container
+# -v "$pwd/jenkins-data":/var/jenkins_home: Create a docker volume in the current subdirectory "jenkins-data" to persist the state of the /var/jenkins_home of the container
+# -v /var/run/docker.sock:/var/run/docker.sock: Allows Jenkins to communicate to the (host) docker daemon to instantiate docker containers
+# -v "$HOME":/home: map local home to jenkins home. TODO: not sure if that's needed/useful...
+# --name jenkins-test: name the container for ease of use
+# jenkinsci/blueocean: the docker image to use (will download if needed)
+docker run \
+  --rm \
+  -u root \
+  -p 8080:8080 \
+  -v "$pwd/jenkins-data":/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --name jenkins-test \
+  jenkinsci/blueocean
