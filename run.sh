96f2eb964d5c225b2dfd6f745e02f1071b61e08b
Julien Marrec <julien.marrec@gmail.com>
2019-11-28 17:11:09 +0100

    Mounting the git directory too

--- run.sh
+++ run.sh
@@ -14,6 +14,7 @@ docker run \
   -u root \
   -p 8080:8080 \
   -v "$(pwd)/jenkins-data":/var/jenkins_home \
+  -v "$(pwd)/git":/home/ubuntu/git \
   -v /var/run/docker.sock:/var/run/docker.sock \
   --name jenkins-test \
   jenkinsci/blueocean
